be3b9d27f4b6f9d58c2cc4032207ef5da537b2d5
nbnbnb <nbnbnb.zhang@gmail.com>
2021-12-19 13:41:13 +0800

    python3

--- crack.py
+++ crack.py
@@ -1,4 +1,4 @@
 oldfile = open("sqlservr.old", "rb").read()
-newfile = oldfile.replace("\x00\x94\x35\x77", "\x00\x80\x84\x1e")
+newfile = oldfile.replace(b"\x00\x94\x35\x77", b"\x00\x80\x84\x1e")
 open("sqlservr", "wb").write(newfile)
 exit()
